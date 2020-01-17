fe47f8d10797a0dbcb20b8a2bb884cc2f0cf6f88
Paul <paul@duhard.com>
2020-01-17 11:30:49 +0100

    marker added from JCDecaux JSON

--- script.js
+++ script.js
@@ -25,83 +25,7 @@ for (var i = 0; i < inputs.length; i++) {
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
 
-// Add pointers (this is were to load my stations coordinates from JCDecaux API)
-var geojson = {
-  type: 'FeatureCollection',
-  features: [
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [4.859900325525473, 45.75373343249737]
-      },
-      properties: {
-        title: 'Mapbox',
-        description:
-          '<strong>Station n°206</strong></br><i>12 rue des Oiseaux plats</i><p> Il reste 2 vélos à cette station</p>'
-      }
-    }
-  ]
-};
-
-// add markers to map (this is were I want to display the stations)
-geojson.features.forEach(function(marker) {
-  // create a HTML element for each feature
-  var el = document.createElement('div');
-  el.className = 'marker';
-
-  // make a marker for each feature and add to the map
-  new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
-});
-
-map.on('load', function() {
-  // When a click event occurs on a feature in the places layer, open a popup at the
-  // location of the feature, with description HTML from its properties.
-  map.on('click', 'places', function(e) {
-    var coordinates = e.features[0].geometry.coordinates.slice();
-    var description = e.features[0].properties.description;
-
-    // Ensure that if the map is zoomed out such that multiple
-    // copies of the feature are visible, the popup appears
-    // over the copy being pointed to.
-    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-    }
-
-    new mapboxgl.Marker(el)
-      .setLngLat(marker.geometry.coordinates)
-      .setPopup(
-        new mapboxgl.Popup({ offset: 25 }) // add popups
-          .setHTML(
-            '<h3>' +
-              marker.properties.title +
-              '</h3><p>' +
-              marker.properties.description +
-              '</p>'
-          )
-      )
-      .addTo(map);
-  });
-
-  // Center the map on the coordinates of any clicked symbol from the 'symbols' layer.
-  // JE DOIS AJOUTER UN LAYER (https://docs.mapbox.com/mapbox-gl-js/example/center-on-symbol/)
-  map.on('click', 'symbols', function(e) {
-    map.flyTo({ center: e.features[0].geometry.coordinates });
-  });
-
-  // Change the cursor to a pointer when the mouse is over the places layer.
-  map.on('mouseenter', 'places', function() {
-    map.getCanvas().style.cursor = 'pointer';
-  });
-
-  // Change it back to a pointer when it leaves.
-  map.on('mouseleave', 'places', function() {
-    map.getCanvas().style.cursor = '';
-  });
-});
-
 // Make an AJAX GET request
-// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
 function ajaxGet(url, callback) {
   var req = new XMLHttpRequest();
   req.open('GET', url);
@@ -119,11 +43,31 @@ function ajaxGet(url, callback) {
   req.send(null);
 }
 
-ajaxGet(
-  'https://api.jcdecaux.com/vls/v3/stations?contract=lyon&apiKey=ceb9bf298a04c4be43e722b64824d650cf690bf3',
-  data => {
-    var stations = JSON.parse(data);
-    console.log(stations[0].name);
-    console.log(stations[0].position.latitude);
-  }
-);
+ajaxGet('stations.json', data => {
+  var stations = JSON.parse(data);
+
+  stations.forEach(function(station) {
+    // create a HTML element for each feature
+    var el = document.createElement('div');
+    el.className = 'marker';
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+      .setLngLat([station.position.longitude, station.position.latitude])
+      .setPopup(
+        new mapboxgl.Popup({ offset: 25 }) // add popups
+          .setHTML(
+            '<h3>' +
+              station.name +
+              '</br>' +
+              station.totalStands.availabilities.bikes +
+              '</h3>'
+          )
+      )
+      .addTo(map);
+  });
+});
+
+// Center the map on the coordinates of any clicked symbol from the 'symbols' layer.
+map.on('click', 'symbols', function(e) {
+  map.flyTo({ center: e.features[0].geometry.coordinates });
+});
